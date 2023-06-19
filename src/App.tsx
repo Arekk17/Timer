17adcbb0fe1f142f712c48338c5f12957bfe8867
Arek Daszkowski <daszkowskiarek145@gmail.com>
2023-06-19 13:03:23 +0200

    repair import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Sidenav from './Components/Navigation/Sidenav';
-import TaskTimer from './Components/Timer/TaskTimer'
+import Sidenav from './Components/Navigation/SideNav';
+import TaskTimer from './Components/Timer/Timer';
 
 const App = () => {
   return (
