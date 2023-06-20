4221b61f80846348c4157ac72c2314ae2ca4da32
Arek Daszkowski <daszkowskiarek145@gmail.com>
2023-06-20 14:25:45 +0200

    api

--- src/MyServices/firebaseServices.ts
+++ src/MyServices/firebaseServices.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRKUcWQCMirs4PwACxOdQCZoXZvWU7DMs",
-  authDomain: "timer-6eb09.firebaseapp.com",
-  projectId: "timer-6eb09",
-  storageBucket: "timer-6eb09.appspot.com",
-  messagingSenderId: "812584187698",
-  appId: "1:812584187698:web:4fb24e8afca1a115fe492b",
-  measurementId: "G-4Y8D49D2RJ"
+  apiKey: "AIzaSyBb07oort0xCP9L4GL-M5JlQCdVv5aeuH0",
+  authDomain: "timer-b4b61.firebaseapp.com",
+  projectId: "timer-b4b61",
+  storageBucket: "timer-b4b61.appspot.com",
+  messagingSenderId: "125627424224",
+  appId: "1:125627424224:web:09d9dbf393650df1485f85",
+  measurementId: "G-FPYD8W3901"
 };
 
 const app = initializeApp(firebaseConfig);
