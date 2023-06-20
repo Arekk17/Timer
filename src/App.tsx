b5d7dce2b84ef60c148732d04da9027edfc4fe7e
Arek Daszkowski <daszkowskiarek145@gmail.com>
2023-06-20 20:38:30 +0200

    adding responsive to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,20 +14,21 @@ const App = () => {
 
   useEffect(() => {
     handleDataRefresh();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <Router>
-      <div className="flex">
+      <div className="flex flex-col md:flex-row">
         <Sidenav />
-        <div className="flex-grow bg-gray-200 flex flex-col">
+        <div className="flex-grow bg-gray-200">
           <Routes>
             {/* Dodaj swoje trasy tutaj */}
             <Route path="/" element={<TaskTimer handleDataRefresh={handleDataRefresh} />} />
             <Route path="/graph" element={<ChartComponent />} />
           </Routes>
-          <div className="w-100 p-2">
+
+          <div className="w-full md:w-3/4 lg:w-4/5 p-2 mx-auto">
             <DataTable refreshData={refreshData} />
           </div>
         </div>
